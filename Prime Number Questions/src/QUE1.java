57251b3e470127765d57385876062ad5c32fe999
mridul2303 <mridul2303@gmail.com>
2025-06-10 11:08:55 +0530

    Adding basic prime number checker program to DSA folder

---
+++ Prime Number Questions/src/QUE1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+//need to find wheather a no. is prime or not
+public class QUE1 {
+    public static void main(String[] args) {
+        System.out.println("enter the no. to check");
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        System.out.println(check(n));
+    }
+    public static String check(int n) {
+        if (n <= 1) return n + " is not prime"; // Edge case
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return n + " is not prime";  // Return immediately if divisible
+            }
+        }
+
+        return n + " is prime";  // If no divisors found, it's prime
+    }
+}
+
