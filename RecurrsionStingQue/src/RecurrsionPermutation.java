5bf6d61315ce967b078d39512d01e60fcb789aa3
mridul2303 <mridul2303@gmail.com>
2025-08-05 15:45:45 +0530

    done some recurrsion questions

---
+++ RecurrsionStingQue/src/RecurrsionPermutation.java
@@ -0,0 +1,19 @@
+public class RecurrsionPermutation {
+    public static void main(String[] args) {
+        permutation("", "abc");
+    }
+
+    static void permutation(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i);
+            permutation(f + ch + s, up.substring(1));
+        }
+    }
+}
